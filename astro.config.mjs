2be391034aee54de013d1efd2bba8866f7be6120
kumar <kumar@talentsavvy.com>
2023-10-06 22:21:34 +0530

    Index Done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), sitemap()],
+	integrations: [sitemap()],
 });
